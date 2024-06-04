0f103e932529f7a704d18f3101d19ada7fbd3da2
Lawi Kiprotich <fleeklawi@gmail.com>
2024-06-04 23:26:07 +0300

    random secret key

--- django_lib/settings.py
+++ django_lib/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY='z)vpzbnfq5!_jpg2tn2kd1d#jiqrt*+6s()k9i=a^8+bvtu3co'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
