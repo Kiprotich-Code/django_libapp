b2c855d7816c7c26933e628f6f6238026a373ca8
Lawi Kiprotich <fleeklawi@gmail.com>
2024-06-04 23:28:46 +0300

    update - django

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 # create a virtual environment named 'venv' if it doesn't already exist
-python3.9 -m venv venv
+python3.10 -m venv venv
 
 # activate the virtual environment
 source venv/bin/activate
