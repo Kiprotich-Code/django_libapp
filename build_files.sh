ca042c057c7b2f56efd911599a61e248fcb27c98
Lawi Kiprotich <fleeklawi@gmail.com>
2024-06-04 22:49:04 +0300

    commits

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,2 @@
-#!/usr/bin/env bash
-
 pip install -r requirements.txt
-
-python manage.py makemigrations --noinput
-python manage.py migrate
-
 python manage.py collectstatic --noinput
--- django_lib/settings.py
+++ django_lib/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".railway.app"]
+ALLOWED_HOSTS = ['127.0.0.1', '.railway.app', '.now.sh', '.vercel.app']
 
 
 # Application definition
@@ -75,20 +75,14 @@ WSGI_APPLICATION = 'django_lib.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.getenv("DB_NAME"),
-#         'USER': os.getenv("DB_USER"),
-#         'PASSWORD': os.getenv("DB_PASSWORD"),
-#         'HOST': os.getenv("DB_HOST"),
-#         'PORT': os.getenv("DB_PORT"),
-#     }
-
-    'default': dj_database_url.config(
-        os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+    }
 }
 
 
